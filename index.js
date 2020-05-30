9805442bac99341d0d609e70ffd4d804709a26e9
Cygnut <ligand@hotmail.co.uk>
2020-05-30 23:30:33 +0100

    tabs2spaces

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-	
-	var d = new Date(); 
-	var s = `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
-	res.send(`Yo dog @ ${s}!`)
+    
+    var d = new Date(); 
+    var s = `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+    res.send(`Yo dog @ ${s}!`)
 })
 
 app.listen(3001, function () {
-	console.log('Example app listening on port 3001!')
+    console.log('Example app listening on port 3001!')
 })
